76265e0cc142c8469a0acd6fd1f4b90c718e9205
grzegorz <gorczykowski@soldevelo.com>
2018-04-12 11:05:46 +0200

    Fixed toast on Android 8.1 and refactored the code using ToastUtils class

--- collect_app/src/main/java/org/odk/collect/android/utilities/ToastUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/ToastUtils.java
@@ -1,9 +1,12 @@
 package org.odk.collect.android.utilities;
 
+import android.annotation.SuppressLint;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.odk.collect.android.application.Collect;
-
 
 public class ToastUtils {
 
@@ -33,5 +36,29 @@ public class ToastUtils {
 
     private static void showToast(int messageResource, int duration) {
         Toast.makeText(Collect.getInstance(), Collect.getInstance().getString(messageResource), duration).show();
+    }
+
+    @SuppressLint("ShowToast")
+    public static void showShortToastInMiddle(int messageResource) {
+        showToastInMiddle(Toast.makeText(Collect.getInstance(), Collect.getInstance().getString(messageResource), Toast.LENGTH_SHORT));
+    }
+
+    @SuppressLint("ShowToast")
+    public static void showShortToastInMiddle(String message) {
+        showToastInMiddle(Toast.makeText(Collect.getInstance(), message, Toast.LENGTH_SHORT));
+    }
+
+    @SuppressLint("ShowToast")
+    public static void showLongToastInMiddle(int messageResource) {
+        showToastInMiddle(Toast.makeText(Collect.getInstance(), Collect.getInstance().getString(messageResource), Toast.LENGTH_LONG));
+    }
+
+    private static void showToastInMiddle(Toast toast) {
+        ViewGroup group = (ViewGroup) toast.getView();
+        TextView messageTextView = (TextView) group.getChildAt(0);
+        messageTextView.setTextSize(21);
+
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
