dde5443bcf1de7f74bddef7ab0d4841805b55d2e
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-03-20 16:41:31 +0100

    Inject analytics to AutoSendWorker

--- collect_app/src/main/java/org/odk/collect/android/upload/AutoSendWorker.java
+++ collect_app/src/main/java/org/odk/collect/android/upload/AutoSendWorker.java
@@ -30,6 +30,7 @@ import androidx.work.WorkerParameters;
 
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.NotificationActivity;
+import org.odk.collect.android.analytics.Analytics;
 import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.dao.FormsDao;
 import org.odk.collect.android.dao.InstancesDao;
@@ -69,6 +70,9 @@ public class AutoSendWorker extends Worker {
 
     @Inject
     StorageMigrationRepository storageMigrationRepository;
+
+    @Inject
+    Analytics analytics;
 
     public AutoSendWorker(@NonNull Context c, @NonNull WorkerParameters parameters) {
         super(c, parameters);
@@ -172,7 +176,7 @@ public class AutoSendWorker extends Worker {
                 String action = protocol.equals(getApplicationContext().getString(R.string.protocol_google_sheets)) ?
                         "HTTP-Sheets auto" : "HTTP auto";
                 String label = Collect.getFormIdentifierHash(instance.getJrFormId(), instance.getJrVersion());
-                Collect.getInstance().logRemoteAnalytics(SUBMISSION, action, label);
+                analytics.logEvent(SUBMISSION, action, label);
             } catch (UploadException e) {
                 Timber.d(e);
                 anyFailure = true;
