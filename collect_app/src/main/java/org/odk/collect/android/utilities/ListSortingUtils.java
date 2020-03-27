b98887ebf1b2e5b0d90711b2b5cda9764e3d72b5
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-03-27 14:16:32 +0100

    Code improvements

--- collect_app/src/main/java/org/odk/collect/android/utilities/ListSortingUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/ListSortingUtils.java
@@ -1,40 +1,38 @@
 package org.odk.collect.android.utilities;
 
-import org.odk.collect.android.provider.InstanceProviderAPI;
+import org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns;
 
 import static org.odk.collect.android.utilities.ApplicationConstants.SortingOrder.BY_DATE_ASC;
 import static org.odk.collect.android.utilities.ApplicationConstants.SortingOrder.BY_DATE_DESC;
-import static org.odk.collect.android.utilities.ApplicationConstants.SortingOrder.BY_NAME_ASC;
 import static org.odk.collect.android.utilities.ApplicationConstants.SortingOrder.BY_NAME_DESC;
 import static org.odk.collect.android.utilities.ApplicationConstants.SortingOrder.BY_STATUS_ASC;
 import static org.odk.collect.android.utilities.ApplicationConstants.SortingOrder.BY_STATUS_DESC;
 
 public class ListSortingUtils {
+    private static String INSTANCES_BY_NAME_ASC = InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE ASC, " + InstanceColumns.STATUS + " DESC";
+    private static String INSTANCES_BY_NAME_DESC = InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE DESC, " + InstanceColumns.STATUS + " DESC";
+    private static String INSTANCES_BY_DATE_ASC = InstanceColumns.LAST_STATUS_CHANGE_DATE + " ASC";
+    private static String INSTANCES_BY_DATE_DESC = InstanceColumns.LAST_STATUS_CHANGE_DATE + " DESC";
+    private static String INSTANCES_BY_STATUS_ASC = InstanceColumns.STATUS + " ASC, " + InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE ASC";
+    private static String INSTANCES_BY_STATUS_DESC = InstanceColumns.STATUS + " DESC, " + InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE ASC";
+
     private ListSortingUtils() {
     }
 
     public static String getInstanceSortingOrder(int selectedSortingOrder) {
-        String sortingOrder = InstanceProviderAPI.InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE ASC, " + InstanceProviderAPI.InstanceColumns.STATUS + " DESC";
         switch (selectedSortingOrder) {
-            case BY_NAME_ASC:
-                sortingOrder = InstanceProviderAPI.InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE ASC, " + InstanceProviderAPI.InstanceColumns.STATUS + " DESC";
-                break;
             case BY_NAME_DESC:
-                sortingOrder = InstanceProviderAPI.InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE DESC, " + InstanceProviderAPI.InstanceColumns.STATUS + " DESC";
-                break;
+                return INSTANCES_BY_NAME_DESC;
             case BY_DATE_ASC:
-                sortingOrder = InstanceProviderAPI.InstanceColumns.LAST_STATUS_CHANGE_DATE + " ASC";
-                break;
+                return INSTANCES_BY_DATE_ASC;
             case BY_DATE_DESC:
-                sortingOrder = InstanceProviderAPI.InstanceColumns.LAST_STATUS_CHANGE_DATE + " DESC";
-                break;
+                return INSTANCES_BY_DATE_DESC;
             case BY_STATUS_ASC:
-                sortingOrder = InstanceProviderAPI.InstanceColumns.STATUS + " ASC, " + InstanceProviderAPI.InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE ASC";
-                break;
+                return INSTANCES_BY_STATUS_ASC;
             case BY_STATUS_DESC:
-                sortingOrder = InstanceProviderAPI.InstanceColumns.STATUS + " DESC, " + InstanceProviderAPI.InstanceColumns.DISPLAY_NAME + " COLLATE NOCASE ASC";
-                break;
+                return INSTANCES_BY_STATUS_DESC;
+            default:
+                return INSTANCES_BY_NAME_ASC;
         }
-        return sortingOrder;
     }
 }
