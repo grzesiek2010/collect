c66c452c9cf4f2b35a21988e91f4ee573ea13801
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-05-14 19:22:17 +0200

    Test

--- collect_app/src/main/java/org/odk/collect/android/preferences/qr/QRScannerFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/qr/QRScannerFragment.java
@@ -12,6 +12,8 @@
  */
 
 package org.odk.collect.android.preferences.qr;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -24,10 +26,14 @@ import timber.log.Timber;
 
 import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.BeepManager;
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
+import org.jetbrains.annotations.NotNull;
 import org.odk.collect.android.R;
 import org.odk.collect.android.preferences.utilities.SettingsUtils;
 import org.odk.collect.android.utilities.CompressionUtils;
@@ -41,19 +47,43 @@ import java.util.List;
 import java.util.zip.DataFormatException;
 
 public class QRScannerFragment extends Fragment implements DecoratedBarcodeView.TorchListener {
+    private static final String ACTION = "action";
 
-    DecoratedBarcodeView barcodeScannerView;
+    public enum Action {BARCODE_WIDGET, APPLY_SETTINGS};
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
     private Button switchFlashlightButton;
     private BeepManager beepManager;
+    private Action action;
+
+    public static QRScannerFragment newInstance(Action action) {
+        QRScannerFragment qrScannerFragment = new QRScannerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ACTION, action);
+        qrScannerFragment.setArguments(bundle);
+        return qrScannerFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_scan, container, false);
+
+        action = (Action) getArguments().getSerializable(ACTION);
+
         beepManager = new BeepManager(getActivity());
         barcodeScannerView = rootView.findViewById(R.id.barcode_view);
         switchFlashlightButton = rootView.findViewById(R.id.switch_flashlight);
-
         barcodeScannerView.setTorchListener(this);
+
+        Intent intent = new IntentIntegrator(getActivity())
+                .setDesiredBarcodeFormats(action == Action.APPLY_SETTINGS ? IntentIntegrator.QR_CODE)
+                .createScanIntent();
+        intent.putExtra(Intents.Scan.SCAN_TYPE, Intents.Scan.MIXED_SCAN);
+
+        capture = new CaptureManager(getActivity(), barcodeScannerView);
+        capture.initializeFromIntent(intent, savedInstanceState);
+        capture.decode();
 
         switchFlashlightButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,7 +99,17 @@ public class QRScannerFragment extends Fragment implements DecoratedBarcodeView.
                     public void barcodeResult(BarcodeResult result) {
                         beepManager.playBeepSoundAndVibrate();
                         try {
-                            SettingsUtils.applySettings(getActivity(), CompressionUtils.decompress(result.getText()));
+                            switch (action) {
+                                case APPLY_SETTINGS:
+                                    SettingsUtils.applySettings(getActivity(), CompressionUtils.decompress(result.getText()));
+                                    break;
+                                case BARCODE_WIDGET:
+                                    Intent returnIntent = new Intent();
+                                    returnIntent.putExtra("SCAN_RESULT", result.getText());
+                                    getActivity().setResult(Activity.RESULT_OK, returnIntent);
+                                    getActivity().finish();
+                                    break;
+                            }
                         } catch (IOException | DataFormatException | IllegalArgumentException e) {
                             Timber.e(e);
                             ToastUtils.showShortToast(getString(R.string.invalid_qrcode));
@@ -100,12 +140,26 @@ public class QRScannerFragment extends Fragment implements DecoratedBarcodeView.
     public void onPause() {
         super.onPause();
         barcodeScannerView.pauseAndWait();
+        capture.onPause();
     }
 
     @Override
     public void onResume() {
         super.onResume();
         barcodeScannerView.resume();
+        capture.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        capture.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NotNull Bundle outState) {
+        capture.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
 
     /**
