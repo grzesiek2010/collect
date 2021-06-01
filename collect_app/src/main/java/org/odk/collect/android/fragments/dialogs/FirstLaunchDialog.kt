239734b40332282b0aa258bba27975d3b59aa72f
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-06-01 19:21:28 +0200

    Implemented AddNewProjectDialog

--- collect_app/src/main/java/org/odk/collect/android/fragments/dialogs/FirstLaunchDialog.kt
+++ collect_app/src/main/java/org/odk/collect/android/fragments/dialogs/FirstLaunchDialog.kt
@@ -12,6 +12,7 @@ import org.odk.collect.android.activities.MainMenuActivity
 import org.odk.collect.android.application.Collect
 import org.odk.collect.android.databinding.FirstLaunchDialogLayoutBinding
 import org.odk.collect.android.injection.DaggerUtils
+import org.odk.collect.android.projects.AddNewProjectDialog
 import org.odk.collect.android.projects.CurrentProjectProvider
 import org.odk.collect.android.projects.ProjectImporter
 import org.odk.collect.android.utilities.DialogUtils
@@ -55,6 +56,13 @@ class FirstLaunchDialog : MaterialFullScreenDialogFragment() {
             getString(R.string.app_name),
             versionInformation.versionToDisplay
         )
+
+        binding.configureViaQrButton.setOnClickListener {
+            DialogUtils.showIfNotShowing(
+                AddNewProjectDialog::class.java,
+                requireActivity().supportFragmentManager
+            )
+        }
 
         binding.configureManuallyButton.setOnClickListener {
             DialogUtils.showIfNotShowing(
