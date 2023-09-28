490af6fdbb48a765ceb799cded52c2baaae42d13
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-09-28 20:07:55 +0200

    Converted ServerSettingsTest to kotlin

---
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/settings/ServerSettingsTest.kt
@@ -0,0 +1,64 @@
+package org.odk.collect.android.feature.settings
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.runner.RunWith
+import org.odk.collect.android.support.TestDependencies
+import org.odk.collect.android.support.pages.MainMenuPage
+import org.odk.collect.android.support.pages.ProjectSettingsPage
+import org.odk.collect.android.support.rules.CollectTestRule
+import org.odk.collect.android.support.rules.TestRuleChain.chain
+import org.odk.collect.androidtest.RecordedIntentsRule
+import org.odk.collect.strings.R
+
+@RunWith(AndroidJUnit4::class)
+class ServerSettingsTest {
+    private val testDependencies = TestDependencies()
+    val rule = CollectTestRule()
+
+    @get:Rule
+    var copyFormChain: RuleChain = chain(testDependencies)
+        .around(RecordedIntentsRule())
+        .around(rule)
+
+    @Test
+    fun whenUsingODKServer_canAddCredentialsForServer() {
+        testDependencies.server.setCredentials("Joe", "netsky")
+        testDependencies.server.addForm("One Question", "one-question", "1", "one-question.xml")
+
+        MainMenuPage().assertOnPage()
+            .openProjectSettingsDialog()
+            .clickSettings()
+            .clickServerSettings()
+            .clickOnURL()
+            .inputText(testDependencies.server.url)
+            .clickOKOnDialog()
+            .assertText(testDependencies.server.url)
+            .clickServerUsername()
+            .inputText("Joe")
+            .clickOKOnDialog()
+            .assertText("Joe")
+            .clickServerPassword()
+            .inputText("netsky")
+            .clickOKOnDialog()
+            .assertText("********")
+            .pressBack(ProjectSettingsPage())
+            .pressBack(MainMenuPage())
+            .clickGetBlankForm()
+            .clickGetSelected()
+            .assertMessage("All downloads succeeded!")
+            .clickOKOnDialog(MainMenuPage())
+    }
+
+    @Test
+    fun selectingServerTypeIsDisabled() {
+        MainMenuPage().assertOnPage()
+            .openProjectSettingsDialog()
+            .clickSettings()
+            .clickServerSettings()
+            .clickOnServerType()
+            .assertTextDoesNotExist(R.string.cancel)
+    }
+}
