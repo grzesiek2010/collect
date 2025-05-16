31c2c3761267c4cc9de09d34c2353fbd182c7968
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-05-17 01:15:27 +0200

    Populate finalized date

--- forms-test/src/main/java/org/odk/collect/formstest/InMemInstancesRepository.java
+++ forms-test/src/main/java/org/odk/collect/formstest/InMemInstancesRepository.java
@@ -142,11 +142,19 @@ public final class InMemInstancesRepository implements InstancesRepository {
                     .build();
         }
 
+        Long currentTime = clock.get();
+
+        if (instance.getStatus().equals(Instance.STATUS_COMPLETE) && instance.getFinalizationDate() == null) {
+            instance = new Instance.Builder(instance)
+                    .finalizationDate(currentTime)
+                    .build();
+        }
+
         Long id = instance.getDbId();
         if (id == null) {
             if (instance.getLastStatusChangeDate() == null) {
                 instance = new Instance.Builder(instance)
-                        .lastStatusChangeDate(clock.get())
+                        .lastStatusChangeDate(currentTime)
                         .build();
             }
 
@@ -159,7 +167,7 @@ public final class InMemInstancesRepository implements InstancesRepository {
         } else {
             if (instance.getDeletedDate() == null) {
                 instance = new Instance.Builder(instance)
-                        .lastStatusChangeDate(clock.get())
+                        .lastStatusChangeDate(currentTime)
                         .build();
             }
 
