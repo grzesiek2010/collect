c4ac42350b109742a39ee1b9867f1ad1fa0a8ae8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-03-06 00:06:33 +0100

    Rework generating upload result message

--- collect_app/src/main/java/org/odk/collect/android/instancemanagement/InstanceUploadResult.kt
+++ collect_app/src/main/java/org/odk/collect/android/instancemanagement/InstanceUploadResult.kt
@@ -1,5 +1,6 @@
 package org.odk.collect.android.instancemanagement
 
+import android.content.res.Resources
 import org.odk.collect.android.instancemanagement.send.FormUploadException
 import org.odk.collect.forms.instances.Instance
 
@@ -9,4 +10,23 @@ sealed class InstanceUploadResult {
     data class Success(override val instance: Instance, val message: String?) : InstanceUploadResult()
 
     data class Error(override val instance: Instance, val exception: FormUploadException) : InstanceUploadResult()
+}
+
+private const val DEFAULT_SUCCESSFUL_TEXT = "full submission upload was successful!"
+
+fun List<InstanceUploadResult>.toMessage(resources: Resources): String = joinToString(separator = "\n\n") { uploadResult ->
+    val name = uploadResult.instance.userVisibleInstanceName(resources)
+
+    val message = when (uploadResult) {
+        is InstanceUploadResult.Success -> uploadResult.message
+        is InstanceUploadResult.Error -> uploadResult.exception.message
+    }?.let {
+        if (it == DEFAULT_SUCCESSFUL_TEXT) {
+            resources.getString(org.odk.collect.strings.R.string.success)
+        } else {
+            it
+        }
+    } ?: ""
+
+    "$name - $message"
 }
